e3218c45da54df0a976d07a6cc88b395ed9c3f9d
RanierDalton <ranier.couto@sptech.school>
2025-05-26 10:25:43 -0300

    feat: Implementar código inicial do processo de ETL com os dados do Pix

--- etl-v1/src/main/java/infra/watch/DadosPix.java
+++ etl-v1/src/main/java/infra/watch/DadosPix.java
@@ -4,9 +4,12 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,8 +27,35 @@ public class DadosPix extends DadosJson{
 
     @Override
     public void processoEtl(String bucket, AmazonS3 s3Client) throws IOException{
-        // Converto esse JSON para uma lista de mapas (um mapa por linha de dados)
-        List<Map<String, Object>> registros = this.mapper();
-        // TODO
+        List<Map<String, Object>> registros = super.mapper();
+        List<Map<String, Object>> registrosPix = new ArrayList<>();
+
+        for(Map<String, Object> registro : registros){
+            // TODO rodar o objeto do pix e ir limpando os campos essenciais que não podem estar vazios e adicionar no registrosPix
+        }
+
+        //[
+        //{
+        //  "AnoMes": 202402,
+        //    "PAG_PFPJ": "PF",
+        //  "REC_PFPJ": "PJ",
+        //"PAG_REGIAO": "SUDESTE",
+        //"REC_REGIAO": "SUDESTE",
+        //"PAG_IDADE": "mais de 60 anos",
+        //"REC_IDADE": "Nao se aplica",
+        //"FORMAINICIACAO": "QRDN",
+        //"NATUREZA": "P2G",
+        //"FINALIDADE": "Pix",
+        //"VALOR": 86135312.46,
+        //"QUANTIDADE": 129997
+        //},
+        //]
+
+        // TODO preparar e enviar arquivo CSV com os campos
+        ByteArrayOutputStream csvOutputStream = super.writeCsv(registrosPix);
+        InputStream csvInputStream = new ByteArrayInputStream(csvOutputStream.toByteArray());
+        String nomeCsv = this.gerarNomeArquivo(null);
+
+        s3Client.putObject(bucket, nomeCsv, csvInputStream, null);
     }
 }
